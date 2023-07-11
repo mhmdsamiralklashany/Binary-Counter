97ff2755b48a176470e920c25c605dd645e443ab
mhmdsamiralklashany <109680061+mhmdsamiralklashany@users.noreply.github.com>
2023-07-11 22:05:25 +0300

    Add files via upload

---
+++ dazzling_leelo1.ino
@@ -0,0 +1,113 @@
+
+
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(7, HIGH);
+  delay(2000); 
+  digitalWrite(7, LOW);
+  delay(2000);
+  
+ // digitalWrite(7, LOW);
+ // delay(2000);
+  digitalWrite(8, HIGH);
+  delay(2000); 
+  digitalWrite(8, LOW);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  //delay(1000); 
+  digitalWrite(8, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(2000);
+  digitalWrite(9, LOW);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(2000);
+
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(2000);
+    digitalWrite(10, LOW);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  delay(2000);
+  
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+  delay(2000);   
+}
